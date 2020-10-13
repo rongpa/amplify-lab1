26ed61ff0cd7a09276edca383aecf6d6773df96b
rongpa <dushyant.pal@gmail.com>
2020-10-13 15:03:52 +1100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-           DUSHYANT PAL MASTER FINAL
+           DUSHYANT PAL -- Develop branch
           </p>
           <a
             className="App-link"
